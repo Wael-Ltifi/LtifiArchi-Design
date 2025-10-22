e38fff8a949e157da3388b41ea57cdeec122e2c9
wael.ltifi <wael.ltifi@talan.com>
2025-10-22 18:41:49 +0100

    build122

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // this gets changed to  base: '/LtifiArchi-Design/', to build something working on git 
+  base: '/LtifiArchi-Design/'
 })
