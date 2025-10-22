59aa0152ca017e9760a6c322a77bf9cda12f33a9
wael.ltifi <wael.ltifi@talan.com>
2025-10-22 17:01:17 +0100

    dirrrrr2

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // this gets changed to  base: '/LtifiArchi-Design/', to build something working on git 
+  base: '/LtifiArchi-Design/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets', // where JS/CSS/images/fonts go
+    rollupOptions: {
+      output: {
+        // make chunk file paths relative
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: ({ name }) => {
+          if (/\.(woff2?|ttf|eot|svg)$/.test(name ?? '')) {
+            return 'assets/fonts/[name]-[hash][extname]'
+          }
+          if (/\.(png|jpe?g|gif|webp)$/.test(name ?? '')) {
+            return 'assets/images/[name]-[hash][extname]'
+          }
+          return 'assets/[name]-[hash][extname]'
+        },
+      },
+    },
+  },
 })
